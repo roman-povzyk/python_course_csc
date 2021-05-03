06d793495777c08ca6087c273e2c6a1243b0f9e3
romanpovzyk <roman.povzyk@gmail.com>
2021-05-03 15:19:24 +0300

    decorators commit

---
+++ 3_decorators.py
@@ -0,0 +1,36 @@
+# @trace
+# def foo(x):
+#     return 42
+#
+#
+# def trace(func):
+#     def inner(*args, **kwargs):
+#         print(func.__name__, args, kwargs)
+#         return func(*args, **kwargs)
+#     return inner()
+#
+#
+# @trace
+# def identity(x):
+#     "I do nothing useful"
+#     return x
+#
+# identity(42)
+
+
+def square(func):
+    return lambda x: func(x * x)
+
+
+def addsome(func):
+    return lambda x: func(x + 42)
+
+
+@addsome
+@square
+def identity(x):
+    return x
+
+print(identity(2))
+
+
