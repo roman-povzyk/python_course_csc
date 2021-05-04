3645af35c017a05897d99d70547ed2a8bbc52aac
romanpovzyk <roman.povzyk@gmail.com>
2021-05-04 11:55:09 +0300

    string commit

---
+++ 4_string.py
@@ -0,0 +1,17 @@
+print("abracadabra".replace('ra', "**", 1))
+print('100500'.isdigit())
+print('foo100500'.isalnum())
+print('foobar'.isalpha())
+
+print(str('я рядок'))
+print(repr('я рядок'))
+print(ascii('я рядок'))
+
+chunk = 'я рядок'.encode('utf-8')
+print(chunk)
+print(chunk.decode('utf-8'))
+
+
+
+
+
