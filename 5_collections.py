36765f736127024a2aced8c064011f3cf59b9bf7
romanpovzyk <roman.povzyk@gmail.com>
2021-05-05 15:33:36 +0300

    string commit

--- 5_collections.py
+++ 5_collections.py
@@ -16,4 +16,36 @@ p = Person('George', age=77)
 print(p._fields)
 print(p.name, p.age)
 
+list = [0] * 3
+print(list)
+
+xs = [1, 2, 3]
+xs.append(42)
+print(xs)
+
+print(xs.index(42))
+xs.insert(0, 4)
+print(xs)
+
+xs = []
+xs += 'abcd'
+print(xs)
+
+xs, ys, zs = {1, 2}, {2, 3}, {3, 4}
+
+a = set.union(xs, ys, zs)
+b = set.intersection(xs, ys, zs)
+c = set.difference(xs, ys, zs)
+
+print(a)
+print(b)
+print(c)
+
+
+
+
+
+
+
+
 
